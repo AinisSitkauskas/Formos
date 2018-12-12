568970a22b553c25233203c97e136c1e723176f5
Ainis Sitkauskas <ainis795@gmail.com>
2018-12-12 19:40:29 +0200

    mokinys.php

---
+++ mokinys.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+ <meta charset="UTF-8">
+ <title>Mokinio pažymiai</title>
+
+  <style>
+  table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    text-align: center;
+    padding: 15px;
+  }
+  </style>
+</head>
+  <body>
+
+<h1>Mokinio pažymiai </h1><br>
+    <form  method = "get" >
+      Mokinio vardas:<br>
+      <input type="text" name="firstName">
+      <br>
+      Mokinio pavardė:<br>
+      <input type="text" name="lastName">
+      <br><br>
+      <input type="submit"  value="Išsaugoti">
+    </form>
+
+<br>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['firstName']) && !empty($_GET['lastName'])) {
+    ?>
+
+
+<table>
+  <tr>
+    <th> Pavardė.</th>
+    <th> Vardas </th>
+    <th> Mokomasis dalykas </th>
+    <th> Pažymys </th>
+    <th> Data </th>
+</tr>
+    <?php
+
+    $studentNames = file_get_contents("studentMarks.csv");
+    $studentNamesArray = explode("\n", $studentNames);
+    $n = count($studentNamesArray);
+    sort($studentNamesArray, SORT_STRING);
+
+    for ($i=1; $i < $n; $i++) {
+            $oneStudentData = $studentNamesArray[$i];
+            $oneStudentDataArray = explode(",", $oneStudentData);
+        if ($_GET['lastName'] == $oneStudentDataArray[0] && $_GET['firstName'] == $oneStudentDataArray[1]) {
+            ?>
+          <tr>
+          <td><?php echo htmlspecialchars($oneStudentDataArray[0]); ?></td>
+          <td><?php echo htmlspecialchars($oneStudentDataArray[1]); ?></td>
+          <td><?php echo htmlspecialchars($oneStudentDataArray[2]); ?></td>
+          <td><?php echo htmlspecialchars($oneStudentDataArray[3]); ?></td>
+          <td><?php echo htmlspecialchars($oneStudentDataArray[4]); ?></td>
+          </tr>
+
+
+              <?php
+        }
+    }
+
+    ?>
+</table>
+    <?php
+}
+?>
+</body>
+</html>
