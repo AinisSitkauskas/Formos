829024a6acb16498aad84452471bb3d0441d3024
Ainis Sitkauskas <ainis795@gmail.com>
2018-12-30 20:26:43 +0200

    pataisytos programos su csv failais

--- desimtukai.php
+++ desimtukai.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="lt">
 <head>
@@ -29,10 +28,9 @@ if (($studentMarks = fopen("studentMarks.csv", "r")) !== false) {
     while (($studentData = fgetcsv($studentMarks, 1000, ",")) !== false) {
         $n = count($studentData);
 
-        for ($i=0; $i < $n; $i++) {
-            if ($studentData[3] == 10) {
+
+        if ($studentData[3] == 10) {
                  array_push($onlyTenStudentArray, $studentData[1].",".$studentData[0].",".$studentData[2].",".$studentData[3].",".$studentData[4]);
-            }
         }
     }
 }
@@ -56,62 +54,3 @@ for ($i=0; $i < $m; $i++) {
 </table>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="lt">
-<head>
- <meta charset="UTF-8">
- <title>Dešimtukų lentelė</title>
-
-  <style>
-  table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-    text-align: center;
-    padding: 15px;
-  }
-  </style>
-</head>
-  <body>
-<table>
-  <tr>
-    <th> Pavardė.</th>
-    <th> Vardas </th>
-    <th> Mokomasis dalykas </th>
-    <th> Pažymys </th>
-    <th> Data </th>
-</tr>
-<?php
-$onlyTenStudentArray = array();
-if (($studentMarks = fopen("studentMarks.csv", "r")) !== false) {
-    while (($studentData = fgetcsv($studentMarks, 1000, ",")) !== false) {
-        $n = count($studentData);
-
-        for ($i=0; $i < $n; $i++) {
-            if ($studentData[3] == 10) {
-                 array_push($onlyTenStudentArray, $studentData[1].",".$studentData[0].",".$studentData[2].",".$studentData[3].",".$studentData[4]);
-            }
-        }
-    }
-}
-    sort($onlyTenStudentArray, SORT_STRING);
-    $m = count($onlyTenStudentArray);
-for ($i=0; $i < $m; $i++) {
-    $oneOnlyTenStudentArray = explode(",", $onlyTenStudentArray[$i]);
-
-    ?>
-
-          <tr>
-          <td><?php echo htmlspecialchars($oneOnlyTenStudentArray[1]); ?></td>
-          <td><?php echo htmlspecialchars($oneOnlyTenStudentArray[0]); ?></td>
-          <td><?php echo htmlspecialchars($oneOnlyTenStudentArray[2]); ?></td>
-          <td><?php echo htmlspecialchars($oneOnlyTenStudentArray[3]); ?></td>
-          <td><?php echo htmlspecialchars($oneOnlyTenStudentArray[4]); ?></td>
-          </tr>
-              <?php
-}
-?>
-</table>
-</body>
-</html>
->>>>>>> 16e9abe05516af35cb2f5b9b45d36b8e84b7101f
