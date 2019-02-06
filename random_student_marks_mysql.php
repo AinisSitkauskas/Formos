479aee6f9e3baab7cc1846d271523648184a66ac
Ainis Sitkauskas <ainis795@gmail.com>
2019-02-06 19:24:17 +0200

    Random student marks mysql

--- random_student_marks_mysql.php
+++ random_student_marks_mysql.php
@@ -1,8 +1,7 @@
 <?php
 include("connection.php");
 if (!empty($error)) {
-    echo $error;
-    die();
+    die($error);
 }
 $n = $argv[1];
 $studentFirstNames = array("Jonas Jonas", "Petras", "Antanas", "Tomas", "Juozas", "Jurgis", "Mantas", "Danielius", "Stasys", "Algis");
@@ -17,37 +16,40 @@ for ($i = 0; $i < $n; $i++) {
     $teachingRandomSubjects[$i] = $teachingSubjects[$teachingRandomSubject];
 }
 $uniqueStudentNames = array();
-foreach ($studentNames as $key => $value) {
-    if (!in_array($value, $uniqueStudentNames)) {
-        $uniqueStudentNames[] = $value;
+foreach ($studentNames as $names) {
+    if (!in_array($names, $uniqueStudentNames)) {
+        $uniqueStudentNames[] = $names;
     }
 }
 $uniqueTeachingRandomSubjects = array();
-foreach ($teachingRandomSubjects as $key => $value) {
-    if (!in_array($value, $uniqueTeachingRandomSubjects)) {
-        $uniqueTeachingRandomSubjects[] = $value;
+foreach ($teachingRandomSubjects as $key => $subject) {
+    if (!in_array($subject, $uniqueTeachingRandomSubjects)) {
+        $uniqueTeachingRandomSubjects[] = $subject;
     }
 }
-foreach ($uniqueStudentNames as $key => $value) {
+foreach ($uniqueStudentNames as $key => $names) {
     $uniqueStudentName = explode(",", $uniqueStudentNames[$key]);
     $sqlQuerry = "INSERT INTO students (name, surname) VALUES ('$uniqueStudentName[0]', '$uniqueStudentName[1]')";
     mysqli_query($connection, $sqlQuerry);
 }
-foreach ($uniqueTeachingRandomSubjects as $key => $value) {
+foreach ($uniqueTeachingRandomSubjects as $key => $subject) {
     $sqlQuerry = "INSERT INTO teaching_subjects (teachingSubject) VALUES ('$uniqueTeachingRandomSubjects[$key]')";
     mysqli_query($connection, $sqlQuerry);
 }
 $counterStudent = count($uniqueStudentNames);
 $counterSubject = count($uniqueTeachingRandomSubjects);
+define("TIME_STAMP_YEAR_2000", "946684800");
+define("TIME_STAMP_YEAR_2019", "1546300800");
 for ($i = 0; $i < $n; $i++) {
     $studentRandomNameId = rand(1, $counterStudent);
     $randomSubjectId = rand(1, $counterSubject);
-    $dateTimestamp = rand(946684800, 1546300800);
+    $dateTimestamp = rand(TIME_STAMP_YEAR_2000, TIME_STAMP_YEAR_2019);
     $mark = rand(1, 10);
     $date = date("Y-m-d", $dateTimestamp);
     $sqlQuerry = "INSERT INTO marks (idStudent, idSubject, mark, date)
     VALUES ('$studentRandomNameId', '$randomSubjectId', '$mark', '$date')";
     mysqli_query($connection, $sqlQuerry);
 }
+
 mysqli_close($connection);
 echo "Pazymiai sugeneruoti ! \n";
--- random_student_marks_mysql2.php
+++ random_student_marks_mysql2.php
@@ -1,10 +1,8 @@
 <?php
 include("connection.php");
 if (!empty($error)) {
-    echo $error;
-    die();
+    die($error);
 }
-
 $n = $argv[1];
 $studentFirstNames = array("Jonas Jonas", "Petras", "Antanas", "Tomas", "Juozas", "Jurgis", "Mantas", "Danielius", "Stasys", "Algis");
 $studentLastNames = array("Kazlauskas", "Jonaitis", "Petraitis", "Minderis", "Ignatavičius", "Kavaliauskas", "Sabonis", "Savickas", "Kesminas", "Adamkus");
@@ -18,32 +16,34 @@ for ($i = 0; $i < $n; $i++) {
     $teachingRandomSubjects[$i] = $teachingSubjects[$teachingRandomSubject];
 }
 $uniqueStudentNames = array();
-foreach ($studentNames as $key => $value) {
-    if (!in_array($value, $uniqueStudentNames)) {
-        $uniqueStudentNames[] = $value;
+foreach ($studentNames as $names) {
+    if (!in_array($names, $uniqueStudentNames)) {
+        $uniqueStudentNames[] = $names;
     }
 }
 $uniqueTeachingRandomSubjects = array();
-foreach ($teachingRandomSubjects as $key => $value) {
-    if (!in_array($value, $uniqueTeachingRandomSubjects)) {
-        $uniqueTeachingRandomSubjects[] = $value;
+foreach ($teachingRandomSubjects as $key => $subject) {
+    if (!in_array($subject, $uniqueTeachingRandomSubjects)) {
+        $uniqueTeachingRandomSubjects[] = $subject;
     }
 }
-foreach ($uniqueStudentNames as $key => $value) {
+foreach ($uniqueStudentNames as $key => $names) {
     $uniqueStudentName = explode(",", $uniqueStudentNames[$key]);
     $sqlQuerry = "INSERT INTO students (name, surname) VALUES ('$uniqueStudentName[0]', '$uniqueStudentName[1]')";
     mysqli_query($connection, $sqlQuerry);
 }
-foreach ($uniqueTeachingRandomSubjects as $key => $value) {
+foreach ($uniqueTeachingRandomSubjects as $key => $subject) {
     $sqlQuerry = "INSERT INTO teaching_subjects (teachingSubject) VALUES ('$uniqueTeachingRandomSubjects[$key]')";
     mysqli_query($connection, $sqlQuerry);
 }
 $counterStudent = count($uniqueStudentNames);
 $counterSubject = count($uniqueTeachingRandomSubjects);
+define("TIME_STAMP_YEAR_2000", "946684800");
+define("TIME_STAMP_YEAR_2019", "1546300800");
 for ($i = 0; $i < $n; $i++) {
     $studentRandomNameId = rand(1, $counterStudent);
     $randomSubjectId = rand(1, $counterSubject);
-    $dateTimestamp = rand(946684800, 1546300800);
+    $dateTimestamp = rand(TIME_STAMP_YEAR_2000, TIME_STAMP_YEAR_2019);
     $mark = rand(1, 10);
     $date = date("Y-m-d", $dateTimestamp);
     $sqlQuerry = "INSERT INTO marks (idStudent, idSubject, mark, date)
@@ -51,71 +51,45 @@ for ($i = 0; $i < $n; $i++) {
     mysqli_query($connection, $sqlQuerry);
 }
 
-$sqlQuery = "SELECT idStudent FROM marks";
-$result = mysqli_query($connection, $sqlQuery);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $idStudents[] = $row["idStudent"];
+function unusedRowDeletion($columnName, $tableName, $counterArrayName) {
+    $sqlQuery = "SELECT '$columnName' FROM marks";
+    $result = mysqli_query($GLOBALS["connection"], $sqlQuery);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $idRow[] = $row["$columnName"];
+        }
+    } else {
+        $error = "Rezultatų nėra";
     }
-} else {
-    $error = "Rezultatų nėra";
-}
-
-for ($i = 1; $i <= $counterStudent; $i++) {
-    if (!in_array($i, $idStudents)) {
-        $sqlQuerry = "DELETE FROM students WHERE id=$i";
-        mysqli_query($connection, $sqlQuerry);
-    }
-}
-
-$sqlQuery = "SELECT idSubject FROM marks";
-$result = mysqli_query($connection, $sqlQuery);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $idSubject[] = $row["idSubject"];
-    }
-} else {
-    $error = "Rezultatų nėra";
-}
-
-for ($i = 1; $i <= $counterSubject; $i++) {
-    if (!in_array($i, $idSubject)) {
-        $sqlQuerry = "DELETE FROM teaching_subjects WHERE id=$i";
-        mysqli_query($connection, $sqlQuerry);
+    for ($i = 1; $i <= $GLOBALS["$counterArrayName"]; $i++) {
+        if (!in_array($i, $idRow)) {
+            $sqlQuerry = "DELETE FROM $tableName WHERE id=$i";
+            mysqli_query($GLOBALS["connection"], $sqlQuerry);
+        }
     }
 }
 
-$sqlQuery = "SELECT id FROM students";
-$result = mysqli_query($connection, $sqlQuery);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $students[] = $row["id"];
+unusedRowDeletion("idStudent", "students", "counterStudent");
+unusedRowDeletion("idSubject", "teaching_subjects", "counterSubject");
+
+function idNumberSorting($tableName) {
+    $sqlQuery = "SELECT id FROM $tableName";
+    $result = mysqli_query($GLOBALS["connection"], $sqlQuery);
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $idRow[] = $row["id"];
+        }
+    } else {
+        $error = "Rezultatų nėra";
     }
-} else {
-    $error = "Rezultatų nėra";
-}
-
-foreach ($students as $key => $value) {
-    $id = $key + 1;
-    $sqlQuerry = "UPDATE students SET id=$id WHERE id=$students[$key]";
-    mysqli_query($connection, $sqlQuerry);
-}
-
-$sqlQuery = "SELECT id FROM teaching_subjects";
-$result = mysqli_query($connection, $sqlQuery);
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $subjects[] = $row["id"];
+    foreach ($idRow as $key => $idRows) {
+        $id = $key + 1;
+        $sqlQuerry = "UPDATE students SET id=$id WHERE id=$idRow[$key]";
+        mysqli_query($GLOBALS["connection"], $sqlQuerry);
     }
-} else {
-    $error = "Rezultatų nėra";
 }
 
-foreach ($subjects as $key => $value) {
-    $id = $key + 1;
-    $sqlQuerry = "UPDATE teaching_subjects SET id=$id WHERE id=$subjects[$key]";
-    mysqli_query($connection, $sqlQuerry);
-}
-
+idNumberSorting("students");
+idNumberSorting("teaching_subjects");
 mysqli_close($connection);
 echo "Pazymiai sugeneruoti ! \n";
